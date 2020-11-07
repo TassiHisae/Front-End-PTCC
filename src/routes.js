eae306ffb2dc750fbb4ecf1af056d8ea40631e5b
Gabriella-C <gabriella.cristinec@gmail.com>
2020-11-07 19:21:00 -0300

    mudanca de rota login, e foi feito a tela cadastro

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,25 @@
-import React from "react";
-import Icon from "react-native-vector-icons/Feather";
-import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import React from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import {
   createBottomTabNavigator,
   createMaterialTopTabNavigator,
-} from "react-navigation-tabs";
-import { createStackNavigator } from "react-navigation-stack";
+} from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
 
-import Back from "./components/Back";
-import Home from "./pages/Home";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import Search from "./pages/Search";
-import Andamento from "./pages/Order/Andamento";
-import Finalizado from "./pages/Order/Finalizado";
-import Settings from "./pages/Profile";
-import Lojas from "./pages/Alimentos/Lojas";
-import Itens from "./pages/Alimentos/Itens";
-import PetShopAcessorios from "./pages/Petshop/Acessorios";
-import PetShopAlimentos from "./pages/Petshop/Alimentos";
-import PetShopSaude from "./pages/Petshop/Saude";
+import Back from './components/Back';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Search from './pages/Search';
+import Andamento from './pages/Order/Andamento';
+import Finalizado from './pages/Order/Finalizado';
+import Settings from './pages/Profile';
+import Lojas from './pages/Alimentos/Lojas';
+import Itens from './pages/Alimentos/Itens';
+import PetShopAcessorios from './pages/Petshop/Acessorios';
+import PetShopAlimentos from './pages/Petshop/Alimentos';
+import PetShopSaude from './pages/Petshop/Saude';
 
 export default (signedIn = false) =>
   createAppContainer(
@@ -38,15 +38,15 @@ export default (signedIn = false) =>
                   },
                   {
                     navigationOptions: ({ navigation }) => ({
-                      title: "ALIMENTOS",
-                      headerTitleAlign: "center",
+                      title: 'ALIMENTOS',
+                      headerTitleAlign: 'center',
                       headerTitleStyle: {
-                        color: "#333",
+                        color: '#333',
                         fontSize: 17,
                       },
                       headerLeft: () => (
                         <Back
-                          onPress={() => navigation.navigate("Home")}
+                          onPress={() => navigation.navigate('Home')}
                           color="#2dc7ff"
                         />
                       ),
@@ -59,9 +59,9 @@ export default (signedIn = false) =>
                     }),
                     tabBarOptions: {
                       labelStyle: {
-                        color: "#000",
-                        fontWeight: "bold",
-                        textTransform: "capitalize",
+                        color: '#000',
+                        fontWeight: 'bold',
+                        textTransform: 'capitalize',
                       },
                       style: {
                         fontSize: 12,
@@ -69,14 +69,14 @@ export default (signedIn = false) =>
                         shadowOpacity: 0,
                         shadowRadius: 0,
                         shadowOffset: { height: 0, width: 0 },
-                        backgroundColor: "#FFF",
+                        backgroundColor: '#FFF',
                       },
                       indicatorStyle: {
-                        backgroundColor: "#f76abc",
+                        backgroundColor: '#f76abc',
                       },
                     },
                     style: {
-                      backgroundColor: "#FFF",
+                      backgroundColor: '#FFF',
                     },
                   }
                 ),
@@ -94,14 +94,14 @@ export default (signedIn = false) =>
                   },
                   {
                     navigationOptions: ({ navigation }) => ({
-                      headerTitleAlign: "center",
+                      headerTitleAlign: 'center',
                       headerTitleStyle: {
-                        color: "#333",
+                        color: '#333',
                         fontSize: 17,
                       },
                       headerLeft: () => (
                         <Back
-                          onPress={() => navigation.navigate("Home")}
+                          onPress={() => navigation.navigate('Home')}
                           color="#2dc7ff"
                         />
                       ),
@@ -114,9 +114,9 @@ export default (signedIn = false) =>
                     }),
                     tabBarOptions: {
                       labelStyle: {
-                        color: "#000",
-                        fontWeight: "bold",
-                        textTransform: "capitalize",
+                        color: '#000',
+                        fontWeight: 'bold',
+                        textTransform: 'capitalize',
                       },
                       style: {
                         fontSize: 12,
@@ -124,14 +124,14 @@ export default (signedIn = false) =>
                         shadowOpacity: 0,
                         shadowRadius: 0,
                         shadowOffset: { height: 0, width: 0 },
-                        backgroundColor: "#FFF",
+                        backgroundColor: '#FFF',
                       },
                       indicatorStyle: {
-                        backgroundColor: "#f76abc",
+                        backgroundColor: '#f76abc',
                       },
                     },
                     style: {
-                      backgroundColor: "#FFF",
+                      backgroundColor: '#FFF',
                     },
                   }
                 ),
@@ -155,10 +155,10 @@ export default (signedIn = false) =>
                       },
                       {
                         navigationOptions: {
-                          title: "PEDIDOS",
-                          headerTitleAlign: "center",
+                          title: 'PEDIDOS',
+                          headerTitleAlign: 'center',
                           headerTitleStyle: {
-                            color: "#333",
+                            color: '#333',
                             fontSize: 17,
                           },
                           headerStyle: {
@@ -170,9 +170,9 @@ export default (signedIn = false) =>
                         },
                         tabBarOptions: {
                           labelStyle: {
-                            color: "#000",
-                            fontWeight: "bold",
-                            textTransform: "capitalize",
+                            color: '#000',
+                            fontWeight: 'bold',
+                            textTransform: 'capitalize',
                           },
                           style: {
                             fontSize: 12,
@@ -180,14 +180,14 @@ export default (signedIn = false) =>
                             shadowOpacity: 0,
                             shadowRadius: 0,
                             shadowOffset: { height: 0, width: 0 },
-                            backgroundColor: "#FFF",
+                            backgroundColor: '#FFF',
                           },
                           indicatorStyle: {
-                            backgroundColor: "#f76abc",
+                            backgroundColor: '#f76abc',
                           },
                         },
                         style: {
-                          backgroundColor: "#FFF",
+                          backgroundColor: '#FFF',
                         },
                       }
                     ),
@@ -207,11 +207,11 @@ export default (signedIn = false) =>
           {
             tabBarOptions: {
               keyboardHidesTabBar: true,
-              activeTintColor: "#000",
-              inactiveTintColor: "rgba(0,0,0,0.6)",
+              activeTintColor: '#000',
+              inactiveTintColor: 'rgba(0,0,0,0.6)',
               style: {
-                backgroundColor: "#fefefe",
-                borderTopColor: "#2dc7ff",
+                backgroundColor: '#fefefe',
+                borderTopColor: '#2dc7ff',
                 borderTopWidth: 2,
                 paddingTop: 4,
               },
@@ -220,6 +220,6 @@ export default (signedIn = false) =>
         ),
       },
 
-      { initialRouteName: "App" }
+      { initialRouteName: 'Sign' }
     )
   );
